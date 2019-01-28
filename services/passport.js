de3450803ddd2056fc5f8764330e105fff05316c
Ross <rostyslavsydorenko@yahoo.com>
2019-01-27 18:02:01 -0800

    Chnage to use Async EC2017

--- services/passport.js
+++ services/passport.js
@@ -1,18 +1,18 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const keys = require("../config/keys"); // Don't have to write keys.js
 
 // Pull out of Mongoose
-const User = mongoose.model('users');
+const User = mongoose.model("users");
 
 passport.serializeUser((user, done) => {
-  done (null, user.id);
+  done(null, user.id);
 });
 passport.deserializeUser((id, done) => {
-  User.findById(id).then (user => {
+  User.findById(id).then(user => {
     done(null, user);
-  })
+  });
 });
 
 passport.use(
@@ -24,21 +24,16 @@ passport.use(
       proxy: true // Trust Proxy and use https
     },
     // done - Whenever we calling that, it tells Passport.js that we're done with OAuth
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then ((existingUser) => {
-            if (existingUser){
-              // We already have a record of this profile.id
-              done(null, existingUser); //This is Done Function for Passport.JS
-              // null shows that it was successfull
-            }
-            else {
-              // New User.Need to make record
-              new User ({ googleId: profile.id })
-              .save()
-              .then (user => done(null, user));
-            }
-          })
-
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        // We already have a record of this profile.id
+        return done(null, existingUser); //This is Done Function for Passport.JS
+        // null shows that it was successfull
+      }
+      // New User.Need to make record
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
